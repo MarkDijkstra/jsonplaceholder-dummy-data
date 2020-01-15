2366a5113b3da29a7fd7959ebad272be8dc9f0e5
Mark Dijkstra <markdijkstra.eu@gmail.com>
2020-01-15 08:34:27 +0100

    fixed wrong variable, added static method and switch to a class

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-module.exports = function random_dummy_data_generator(route, limit) {
-    if (typeof route === "undefined" || typeof route !== 'string') {
-        route = '/posts';//default path
-    } else if (route.substring(0, 1) !== "/") {
-        route = '/'+route;
+module.exports = class random_dummy_data_generator{
+    static get(route, limit)
+    {
+        if (typeof route === "undefined" || typeof route !== 'string') {
+            route = '/posts';//default path
+        } else if (route.substring(0, 1) !== "/") {
+            route = '/' + route;
+        }
+        if (typeof limit === "undefined" || isNaN(limit)) {
+            limit = 5000;//max results
+        }
+        return fetch('https://jsonplaceholder.typicode.com' + route)
+            .then((resp) => resp.json())
+            .then(function (d) {
+                return d.slice(0, limit);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
-    if (typeof limit === "undefined" || isNaN(route)) {
-        limit = 5000;//max results
-    }
-    return fetch('https://jsonplaceholder.typicode.com'+route)
-        .then((resp) => resp.json())
-        .then(function(d) {
-            return d.slice(0, limit);
-        })
-        .catch(function(error) {
-            console.log(error);
-        });
 };
